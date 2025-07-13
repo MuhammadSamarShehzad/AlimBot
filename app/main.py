663fb458f6d0b09b47985164246635d0ad3ebdab
muhammadsamarshehzad <samarshehzad598@gmail.com>
2025-07-13 08:48:41 +0500

    render file updated

--- app/main.py
+++ app/main.py
@@ -2,7 +2,8 @@ import streamlit as st
 import requests
 import random
 
-API_URL = "http://fastapi:8000/query"
+# API_URL = "http://fastapi:8000/query"
+API_URL = "https://fastapi-backend.onrender.com/query"
 
 st.set_page_config(page_title="Islamic Guidance AI", page_icon="🕌", layout="centered")
 
